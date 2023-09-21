eeb792e8326d1e67a8501c02f96d02eef9454c97
Peter Lewandowski <96852364+peterlewandowski@users.noreply.github.com>
2023-09-20 23:57:20 -0400

    added inter font variable to app.tsx and tailwind config #time 1h

--- apps/frontend-mentor/bmi-calculator/tailwind.config.js
+++ apps/frontend-mentor/bmi-calculator/tailwind.config.js
@@ -8,7 +8,11 @@ module.exports = {
         ...createGlobPatternsForDependencies(__dirname),
     ],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                inter: "var(--font-inter}",
+            },
+        },
     },
     plugins: [],
 };
