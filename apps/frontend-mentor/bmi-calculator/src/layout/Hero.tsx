9541ea7219ee3b125aa627421e86b94e41d6de44
Peter Lewandowski <96852364+peterlewandowski@users.noreply.github.com>
2023-09-26 23:31:00 -0400

    added custom fontSizes per figma, renamed TopSection to Hero #time 1h

---
+++ apps/frontend-mentor/bmi-calculator/src/layout/Hero.tsx
@@ -0,0 +1,8 @@
+export const Hero = () => {
+    return (
+        <div className="flex [&>*]:basis-1/2">
+            <div className="text-heading-xl">Body Mass Index Calculator</div>
+            <div className="text-heading-m">Enter your details below</div>
+        </div>
+    );
+};
