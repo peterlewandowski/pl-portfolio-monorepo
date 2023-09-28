ea7de08d24b2081b84fcb7b0f3883c1e86adc4fe
Peter Lewandowski <96852364+peterlewandowski@users.noreply.github.com>
2023-09-27 21:48:03 -0400

    added hero body text and color #time 15m

--- apps/frontend-mentor/bmi-calculator/src/layout/Hero.tsx
+++ apps/frontend-mentor/bmi-calculator/src/layout/Hero.tsx
@@ -1,8 +1,16 @@
 export const Hero = () => {
     return (
         <div className="flex [&>*]:basis-1/2">
-            <div className="text-heading-xl">Body Mass Index Calculator</div>
-            <div className="text-heading-m">Enter your details below</div>
+            <div className="">
+                <div className="text-heading-xl">Body Mass Index Calculator</div>
+                <div className="text-body-m text-electricBlue">
+                    Better understand your weight in relation to your height using our body mass
+                    index (BM) calculator. While BMI is not the sole determinant of a healthy
+                    weight, it offers a valuable starting point to evaluate your overall health and
+                    well-being.
+                </div>
+            </div>
+            <div className="text-heading-m ">Enter your details below</div>
         </div>
     );
 };
