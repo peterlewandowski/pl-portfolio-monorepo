6736902dc7b5b667e10553af61488d0936e4c9fc
Peter Lewandowski <96852364+peterlewandowski@users.noreply.github.com>
2023-10-05 23:24:16 -0400

    added LeftCurve svg with initial styling #time 30m

--- apps/frontend-mentor/bmi-calculator/src/layout/WhatYourBMI.tsx
+++ apps/frontend-mentor/bmi-calculator/src/layout/WhatYourBMI.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
 import man from "../images/image-man-eating.webp";
+import { ReactComponent as LeftCurve } from "../images/pattern-curved-line-left.svg";
 
 export const WhatYourBMI = () => {
     return (
         <div className="mt-52 flex gap-4 [&>*]:basis-1/2">
             <div className="h-96 w-96 rounded-3xl bg-sky-100">
-                <Image src={man} alt="A man eating sushi" width={468} height={533} />
+                <Image src={man} alt="A man eating sushi" width={468} height={533} />L
             </div>
-            <div className="*******">
+            <div className="flex flex-col">
+                <LeftCurve className="self-end" />
                 <h1 className="text-heading-l">What your BMI result means</h1>
                 <div className="text-body-m text-electricBlue">
                     A BMI range of 18.5 to 24.9 is considered a &apos;healthy weight.&apos;
