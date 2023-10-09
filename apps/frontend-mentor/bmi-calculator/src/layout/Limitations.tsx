a6568b1bb698dca9519d5dd3dd6e1837c46f6734
Peter Lewandowski <96852364+peterlewandowski@users.noreply.github.com>
2023-10-08 23:48:49 -0400

    added Card component and cardData for mapping out the info cards in the Limitations section #time 1h

---
+++ apps/frontend-mentor/bmi-calculator/src/layout/Limitations.tsx
@@ -0,0 +1,68 @@
+import { Card } from "../components/Card";
+import { Container } from "../components/Container";
+import { ReactComponent as AgeIcon } from "../images/icon-age.svg";
+import { ReactComponent as GenderIcon } from "../images/icon-gender.svg";
+import { ReactComponent as MuscleIcon } from "../images/icon-muscle.svg";
+import { ReactComponent as PregnancyIcon } from "../images/icon-pregnancy.svg";
+import { ReactComponent as RaceIcon } from "../images/icon-race.svg";
+
+const cardsData = [
+    {
+        icon: <GenderIcon />,
+        title: "Gender",
+        content:
+            "The development and body fat composition of girls and boys vary with age. Consequently, a child's age and gender are considered when evaluating their BMI.",
+    },
+    {
+        icon: <AgeIcon />,
+        title: "Age",
+        content:
+            "In aging individuals, increased body fat and muscle loss may cause BMI to underestimate body fat content.",
+    },
+    {
+        icon: <MuscleIcon />,
+        title: "Muscle",
+        content:
+            "BMI may misclassify muscular individuals as overweight or obese, as it doesn't differentiate muscle from fat.",
+    },
+    {
+        icon: <PregnancyIcon />,
+        title: "Pregnancy",
+        content:
+            "Expectant mothers experience weight gain due to their growing baby. Maintaining a healthy pre-pregnancy BMI is advisable to minimise health risks for both mother and child.",
+    },
+    {
+        icon: <RaceIcon />,
+        title: "Race",
+        content:
+            "Certain health concerns may affect individuals of some Black and Asian origins at lower BMIs than others. To learn more, it is advised to discuss this with your GP or practice nurse.",
+    },
+];
+
+export const Limitations = () => {
+    return (
+        <section id="limitations">
+            <Container className="flex flex-col">
+                <div className="flex gap-32">
+                    <div>
+                        <h1 className="text-heading-l">Limitations of BMI</h1>
+                        <p className="text-body-m">
+                            Although BMI is often a practical indicator of healthy weight, it is not
+                            suited for every person. Specific groups should carefully consider their
+                            BMI outcomes, and in certain cases, the measurement may not be
+                            beneficial to use.
+                        </p>
+                    </div>
+                    {cardsData.map((item) => (
+                        <Card
+                            key={item.title}
+                            icon={item.icon}
+                            title={item.title}
+                            content={item.content}
+                        />
+                    ))}
+                </div>
+            </Container>
+        </section>
+    );
+};
