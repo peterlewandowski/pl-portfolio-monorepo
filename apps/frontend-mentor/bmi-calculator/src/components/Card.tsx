9ad52b454ef37c444451f1d9378ec37abb1094dc
Peter Lewandowski <96852364+peterlewandowski@users.noreply.github.com>
2023-10-09 22:42:11 -0400

    adjusted styling on Card and Limitations section #time 1h

--- apps/frontend-mentor/bmi-calculator/src/components/Card.tsx
+++ apps/frontend-mentor/bmi-calculator/src/components/Card.tsx
@@ -6,10 +6,10 @@ type Props = {
 
 export const Card: React.FC<Props> = ({ icon, title, content }) => {
     return (
-        <div className="inline-flex w-[22.8125rem] flex-col items-start justify-start gap-8 rounded-2xl bg-white p-8 shadow">
-            <div className="flex h-40 flex-col">
-                <div className="inline-flex items-center justify-start gap-4 self-stretch">
-                    <div className="relative h-8 w-8">{icon}</div>
+        <div className="flex w-[22.8125rem] shrink-0 flex-col rounded-2xl bg-white p-8 shadow-[16px_32px_56px_0_rgba(143,174,207,0.25)]">
+            <div className="flex flex-col">
+                <div className="mb-4 flex items-center justify-start gap-4">
+                    <div className="h-8 w-8">{icon}</div>
                     <div className="text-heading-s text-gunmetal">{title}</div>
                 </div>
                 <div className="text-electricBlue">{content}</div>
