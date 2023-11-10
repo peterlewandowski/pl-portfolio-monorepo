ef74b7a46a1bb3a7c4c69c556f3161aea049fd9a
Peter Lewandowski <96852364+peterlewandowski@users.noreply.github.com>
2023-11-09 22:51:40 -0500

    Revert "Bmi calculator (#7)"
    This reverts commit 889b2a3fec7c8887844f762d3c52404d866b0a7b.

--- apps/frontend-mentor/bmi-calculator/pages/_app.tsx
+++ apps/frontend-mentor/bmi-calculator/pages/_app.tsx
@@ -1,21 +1,14 @@
 import { AppProps } from "next/app";
-import Inter from "next/font/local";
 import Head from "next/head";
 import "./styles.css";
-
-const inter = Inter({
-    src: [{ path: "../src/fonts/Inter-VariableFont_slnt,wght.ttf" }],
-    variable: "--font-inter",
-});
 
 function CustomApp({ Component, pageProps }: AppProps) {
     return (
         <>
             <Head>
-                <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-                <title>BMI Calculator Site</title>
+                <title>Welcome to frontend-mentor/bmi-calculator!</title>
             </Head>
-            <main className={inter.variable}>
+            <main className="app">
                 <Component {...pageProps} />
             </main>
         </>
