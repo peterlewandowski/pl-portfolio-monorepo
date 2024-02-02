f6bd69816f1686f5ba3478c01f151df58ec43011
Peter Lewandowski <96852364+peterlewandowski@users.noreply.github.com>
2024-02-01 23:44:28 -0500

    added radio input from daisyUI #time 1h

--- apps/frontend-mentor/bmi-calculator/src/layout/Hero.tsx
+++ apps/frontend-mentor/bmi-calculator/src/layout/Hero.tsx
@@ -27,16 +27,23 @@ export const Hero = () => {
                         </div>
                         <div className="inline-flex items-start justify-start gap-6 self-stretch">
                             <div className="flex h-8 shrink grow basis-0 items-center justify-start gap-4">
-                                <div className="relative h-8 w-8">
-                                    <div className="absolute left-0 top-0 h-8 w-8 rounded-full border bg-blue-600 bg-opacity-20" />
-                                    <div className="absolute left-[8px] top-[8px] h-3.5 w-3.5 rounded-full bg-blue-600" />
-                                </div>
+                                <input
+                                    type="radio"
+                                    name="radio-10"
+                                    className="radio checked:bg-blue-600"
+                                    defaultChecked
+                                />
+
                                 <div className="font-['Inter'] text-base font-semibold leading-normal text-slate-800">
                                     Metric
                                 </div>
                             </div>
                             <div className="flex h-8 shrink grow basis-0 items-center justify-start gap-4">
-                                <div className="h-8 w-8 rounded-full border border-zinc-200 bg-white" />
+                                <input
+                                    type="radio"
+                                    name="radio-10"
+                                    className="radio checked:bg-blue-600"
+                                />
                                 <div className="font-['Inter'] text-base font-semibold leading-normal text-slate-800">
                                     Imperial
                                 </div>
@@ -70,7 +77,7 @@ export const Hero = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className="inline-flex items-center justify-center gap-6 self-stretch rounded-tl-2xl rounded-tr-full rounded-bl-2xl rounded-br-full bg-gradient-to-r from-blue-600 to-indigo-500 p-8">
+                        <div className="inline-flex items-center justify-center gap-6 self-stretch rounded-bl-2xl rounded-br-full rounded-tl-2xl rounded-tr-full bg-gradient-to-r from-blue-600 to-indigo-500 p-8">
                             <div className="inline-flex shrink grow basis-0 flex-col items-start justify-center gap-2">
                                 <div className="self-stretch font-['Inter'] text-base font-semibold leading-normal text-white">
                                     Your BMI is...
