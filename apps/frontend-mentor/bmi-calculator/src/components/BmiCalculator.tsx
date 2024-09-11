5480b9937c547c8fbf1105f5486e45969a0cdab3
Peter Lewandowski <96852364+peterlewandowski@users.noreply.github.com>
2024-09-10 23:50:21 -0400

    chore: update bmiResult calculations and cleanup in bmi-calculator

--- apps/frontend-mentor/bmi-calculator/src/components/BmiCalculator.tsx
+++ apps/frontend-mentor/bmi-calculator/src/components/BmiCalculator.tsx
@@ -1,32 +1,48 @@
 import { useCallback, useEffect, useState } from "react";
+import { ImperialForm } from "./ImperialForm";
+import { MetricForm } from "./MetricForm";
 
-type InputsState = {
-    height: number;
-    weight: number;
-};
+export interface FormInputs {
+    cm: string;
+    kg: string;
+    ft: string;
+    inches: string;
+    st: string;
+    lbs: string;
+}
 
 export const BmiCalculator = () => {
-    const [inputs, setInputs] = useState<InputsState>({ weight: 0, height: 0 });
-    const [bmiResult, setBmiResult] = useState<string | null>(null);
     const [unit, setUnit] = useState("metric");
-    console.log("unit: ", unit);
+    const [inputs, setInputs] = useState<FormInputs>({
+        cm: "",
+        kg: "",
+        ft: "",
+        inches: "",
+        st: "",
+        lbs: "",
+    });
+    const [bmiResult, setBmiResult] = useState<string>("");
 
     const handleUnitChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setUnit(e.target.value);
+        setBmiResult("");
     };
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
-        setInputs((prevState) => ({ ...prevState, [name]: value ? Number(value) : "" }));
+        setInputs((prevState) => ({ ...prevState, [name]: value }));
     };
 
     const calculateBMI = useCallback(() => {
-        const { height, weight } = inputs;
-        if (weight && height) {
-            const bmiValue = weight / (height / 100) ** 2;
-            setBmiResult(bmiValue.toFixed(1));
-        } else {
-            setBmiResult(null);
+        const { cm, kg, ft, inches, st, lbs } = inputs;
+        if (unit === "metric" && cm && kg) {
+            const bmiMetric = Number(kg) / (Number(cm) / 100) ** 2;
+            setBmiResult(bmiMetric.toFixed(1));
+        } else if (unit === "imperial" && ft && inches && st && lbs) {
+            const heightInches = Number(ft) * 12 + Number(inches);
+            const weightLbsTotal = Number(st) * 14 + Number(lbs);
+            const bmiImperial = (weightLbsTotal / heightInches ** 2) * 703;
+            setBmiResult(bmiImperial.toFixed(1));
         }
     }, [inputs]);
 
@@ -35,7 +51,7 @@ export const BmiCalculator = () => {
     }, [calculateBMI]);
 
     return (
-        <div className="inline-flex h-[30rem] w-96 flex-col items-start justify-start gap-8 rounded-2xl bg-white p-8 shadow-[16px_32px_56px_0_rgba(143,174,207,0.25)]">
+        <div className="inline-flex w-96 flex-col items-start justify-start gap-8 rounded-2xl bg-white p-8 shadow-[16px_32px_56px_0_rgba(143,174,207,0.25)]">
             <div className="flex flex-col items-start justify-start gap-2">
                 <div className="font-['Inter'] text-2xl font-semibold text-slate-800">
                     Enter your details below
@@ -71,68 +87,11 @@ export const BmiCalculator = () => {
             </div>
             <div className="inline-flex items-start justify-start gap-6 self-stretch">
                 <div className="inline-flex shrink grow basis-0 flex-col items-start justify-start gap-2">
-                    <div className="font-['Inter'] text-sm font-normal leading-tight text-slate-500">
-                        Height
-                    </div>
                     {unit === "metric" ? (
-                        <div className="inline-flex items-center justify-start gap-6 self-stretch rounded-xl border border-zinc-200 bg-white px-6 py-5">
-                            <input
-                                name="height"
-                                type="number"
-                                placeholder="0"
-                                className="input input-ghost w-full max-w-xs"
-                                onChange={handleInputChange}
-                            />
-                            <div className="font-['Inter'] text-2xl font-semibold text-blue-600">
-                                cm
-                            </div>
-                        </div>
+                        <MetricForm formValues={inputs} handleInputChange={handleInputChange} />
                     ) : (
-                        <div className="flex gap-6">
-                            <div className="inline-flex items-center justify-start gap-6 self-stretch rounded-xl border border-zinc-200 bg-white px-6 py-5">
-                                <input
-                                    name="height"
-                                    type="number"
-                                    placeholder="0"
-                                    className="input input-ghost w-full max-w-xs"
-                                    onChange={handleInputChange}
-                                />
-                                <div className="font-['Inter'] text-2xl font-semibold text-blue-600">
-                                    ft
-                                </div>
-                            </div>
-
-                            <div className="inline-flex items-center justify-start gap-6 self-stretch rounded-xl border border-zinc-200 bg-white px-6 py-5">
-                                <input
-                                    name="height"
-                                    type="number"
-                                    placeholder="0"
-                                    className="input input-ghost w-full max-w-xs"
-                                    onChange={handleInputChange}
-                                />
-                                <div className="font-['Inter'] text-2xl font-semibold text-blue-600">
-                                    in
-                                </div>
-                            </div>
-                        </div>
+                        <ImperialForm formValues={inputs} handleInputChange={handleInputChange} />
                     )}
-                </div>
-                <div className="inline-flex shrink grow basis-0 flex-col items-start justify-start gap-2">
-                    <div className="font-['Inter'] text-sm font-normal leading-tight text-slate-500">
-                        Weight
-                    </div>
-                    <div className="inline-flex items-center justify-start gap-6 self-stretch rounded-xl border border-zinc-200 bg-white px-6 py-5">
-                        <input
-                            name="weight"
-                            type="number"
-                            placeholder="0"
-                            className="input input-ghost w-full max-w-xs"
-                            onChange={handleInputChange}
-                        />
-                        <div className="font-['Inter'] text-2xl font-semibold text-blue-600">
-                            kg
-                        </div>
-                    </div>
                 </div>
             </div>
             <div className="inline-flex items-center justify-center gap-6 self-stretch rounded-l-2xl rounded-r-[10rem] bg-gradient-to-r from-blue-600 to-indigo-500 p-8">
