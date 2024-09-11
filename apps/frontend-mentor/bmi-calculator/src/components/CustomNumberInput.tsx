27083655f606b7d01b421722ae87aec687bdc809
Peter Lewandowski <96852364+peterlewandowski@users.noreply.github.com>
2024-09-10 23:46:25 -0400

    chore: update CustomNumberInput in bmi-calculator

--- apps/frontend-mentor/bmi-calculator/src/components/CustomNumberInput.tsx
+++ apps/frontend-mentor/bmi-calculator/src/components/CustomNumberInput.tsx
@@ -1,19 +1,21 @@
 import { InputHTMLAttributes } from "react";
 
-interface NumberInputProps extends InputHTMLAttributes<HTMLInputElement> {
-    inputUnit: string;
-}
+interface NumberInputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
-export const CustomNumberInput = ({ inputUnit, ...props }: NumberInputProps) => {
+export const CustomNumberInput = ({ name, ...props }: NumberInputProps) => {
     return (
         <div className="flex items-center justify-start gap-6 self-stretch rounded-xl border border-zinc-200 bg-white px-6 py-5">
             <input
-                className="input input-ghost w-full max-w-xs"
+                name={name}
                 type="number"
                 placeholder="0"
+                min="0"
+                className="input input-ghost w-full max-w-xs"
                 {...props}
             />
-            <p className="font-['Inter'] text-2xl font-semibold text-blue-600">{inputUnit}</p>
+            <p className="font-['Inter'] text-2xl font-semibold text-blue-600">
+                {name?.slice(0, 2)}
+            </p>
         </div>
     );
 };
