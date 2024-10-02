4145b28321d243a9893572bc1ad01b10b56ae648
Peter Lewandowski <96852364+peterlewandowski@users.noreply.github.com>
2024-10-01 23:56:31 -0400

    bmi-calculator input border color on hover

--- apps/frontend-mentor/bmi-calculator/src/components/CustomNumberInput.tsx
+++ apps/frontend-mentor/bmi-calculator/src/components/CustomNumberInput.tsx
@@ -4,18 +4,16 @@ interface NumberInputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
 export const CustomNumberInput = ({ name, ...props }: NumberInputProps) => {
     return (
-        <div className="flex items-center justify-start gap-6 self-stretch rounded-xl border border-blue-500 bg-white px-6 py-5">
+        <div className="flex items-center justify-start gap-6 self-stretch rounded-xl border border-gray-200 bg-white px-6 py-5 hover:border-blue-500">
             <input
                 name={name}
                 type="number"
                 placeholder="0"
                 min="0"
-                className="input input-ghost w-full max-w-xs"
+                className="input w-full max-w-xs"
                 {...props}
             />
-            <p className="text-2xl font-semibold text-blue-600">
-                {name?.slice(0, 2)}
-            </p>
+            <p className="text-2xl font-semibold text-blue-600">{name?.slice(0, 2)}</p>
         </div>
     );
 };
